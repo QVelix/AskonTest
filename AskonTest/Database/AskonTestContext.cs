9db67be83ced46ffec9bb10ada059fae4cec1764
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-07-23 15:30:19 +0400

    Создал контекст бд

---
+++ AskonTest/Database/AskonTestContext.cs
@@ -0,0 +1,16 @@
+﻿using AskonTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AskonTest.Database;
+
+public class AskonTestContext : DbContext
+ {
+ 	public AskonTestContext(DbContextOptions<AskonTestContext> options) : base(options)
+ 	{
+ 	}
+    
+    public virtual DbSet<Applications> Applications { get; set; }
+    public virtual DbSet<Settings> Settings { get; set; }
+    public virtual DbSet<Users> Users { get; set; }
+    public virtual DbSet<SettingsStatuses> SettingsStatuses { get; set; }
+ }
