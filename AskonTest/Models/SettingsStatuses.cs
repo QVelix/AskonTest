20f1f34e5db7de0244553f4b1c95dbe6e2992b16
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-07-23 15:23:27 +0400

    Добавил описание модели статуса настроек со связью многие-ко-многим между настройками, приложениями и пользователями

--- AskonTest/Models/SettingsStatuses.cs
+++ AskonTest/Models/SettingsStatuses.cs
@@ -2,5 +2,11 @@
 
 public class SettingsStatuses
 {
-	
+	public string Status { get; set; }
+	public long ApplicationId { get; set; }
+	public long? UserId { get; set; } = null;
+	public long SettingsId { get; set; }
+	public Applications Application { get; set; } = null!;
+	public Users? User { get; set; } = null;
+	public Settings Settings { get; set; } = null!;
 }
